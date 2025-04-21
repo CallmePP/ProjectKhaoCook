be8ec4a5eeb7c7e226aff6f04dec2c31e115a3ff
Plakraphong <maxkanicrc@gmail.com>
2025-04-22 04:52:36 +0700

    updateTabbar

--- screens/CustomTabBar.js
+++ screens/CustomTabBar.js
@@ -1,13 +1,16 @@
 import React from 'react';
-import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
-import Ionicons from 'react-native-vector-icons/Ionicons';
+import { View, Text, TouchableOpacity, StyleSheet, Image } from 'react-native';
 
-const TabBarItem = ({ label, icon, onPress, isActive }) => {
+const TabBarItem = ({ label, icon, iconActive, onPress, isActive }) => {
   const color = isActive ? '#d36c00' : '#ccc';
 
   return (
     <TouchableOpacity style={{ alignItems: 'center' }} onPress={onPress}>
-      <Ionicons name={icon} size={24} color={color} />
+      <Image
+        source={isActive ? iconActive : icon}
+        style={styles.icon}
+        resizeMode="contain"
+      />
       <Text style={{ fontSize: 12, color }}>{label}</Text>
     </TouchableOpacity>
   );
@@ -18,31 +21,36 @@ const CustomTabBar = ({ state, navigation }) => {
     <View style={styles.tabBar}>
       <TabBarItem
         label="Home"
-        icon="home"
+        icon={require('../assets/f.png')}
+        iconActive={require('../assets/search-interface-symbol 1.png')}
         onPress={() => navigation.navigate('Home')}
         isActive={state.index === 0}
       />
       <TabBarItem
         label="เก็บสูตร"
-        icon="bookmark"
+        icon={require('../assets/s.png')}
+        iconActive={require('../assets/diary 1.png')}
         onPress={() => navigation.navigate('SavedRecipes')}
         isActive={state.index === 1}
       />
       <TabBarItem
         label="สุ่มเมนู"
-        icon="restaurant"
+        icon={require('../assets/r.png')}
+        iconActive={require('../assets/restaurant 2.png')}
         onPress={() => navigation.navigate('RandomMenuScreen')}
         isActive={state.index === 2}
       />
       <TabBarItem
         label="แจ้งเตือน"
-        icon="notifications"
+        icon={require('../assets/n.png')}
+        iconActive={require('../assets/bell 1.png')}
         onPress={() => navigation.navigate('Notifications')}
         isActive={state.index === 3}
       />
       <TabBarItem
         label="โปรไฟล์"
-        icon="person"
+        icon={require('../assets/p.png')}
+        iconActive={require('../assets/user 1.png')}
         onPress={() => navigation.navigate('Profile')}
         isActive={state.index === 4}
       />
@@ -58,8 +66,13 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     height: 60,
     borderTopWidth: 1,
-    borderTopColor: '#ccc'
-  }
+    borderTopColor: '#ccc',
+  },
+  icon: {
+    width: 24,
+    height: 24,
+    marginBottom: 4,
+  },
 });
 
 export default CustomTabBar;
